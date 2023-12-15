317e6bef4121f655febbf94ca32916360455e56e
Adisie Fenta <adisiefenta23@gmail.com>
2023-12-15 10:07:05 +0300

    some changes

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,5 +1,9 @@
 
 import { FaRegHandshake } from "react-icons/fa"
+import { FaDesktop } from "react-icons/fa"
+import { SiServerless } from "react-icons/si"
+import { FaDatabase } from "react-icons/fa"
+import { IoSchoolSharp } from "react-icons/io5"
 
 const Services = () => {
   return (
@@ -7,27 +11,34 @@ const Services = () => {
         <h3><FaRegHandshake className="service-icon"/>Services</h3>
         <div className="service-list">
             <div className="service">
+                <h4>Frontend Development</h4>
+                <FaDesktop className="icon"/>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptatem totam nulla vitae, numquam dolore quaerat praesentium! Consequuntur iste nisi quod nemo, 
+                </p>
+            </div>
+
+            <div className="service">
                 <h4>Backend Development</h4>
+                <SiServerless className="icon"/>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptatem totam nulla vitae, numquam dolore quaerat praesentium! Consequuntur iste nisi quod nemo, qui perspiciatis quibusdam beatae! Minus, quam natus. Exercitationem, eaque obcaecati.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptatem totam nulla vitae, numquam dolore quaerat praesentium! Consequuntur iste nisi quod nemo, 
                 </p>
             </div>
+            
             <div className="service">
-                <h4>UI/UX DESIGNING</h4>
+                <h4>Database Management</h4>
+                <FaDatabase className="icon"/>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptatem totam nulla vitae, numquam dolore quaerat praesentium! Consequuntur iste nisi quod nemo, qui perspiciatis quibusdam beatae! Minus, quam natus. Exercitationem, eaque obcaecati.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptatem totam nulla vitae, numquam dolore quaerat praesentium! Consequuntur iste nisi quod nemo, 
                 </p>
             </div>
+            
             <div className="service">
-                <h4>DESIGN IMPLEMENTATION</h4>
+                <h4>Tutoring</h4>
+                <IoSchoolSharp className="icon"/>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptatem totam nulla vitae, numquam dolore quaerat praesentium! Consequuntur iste nisi quod nemo, qui perspiciatis quibusdam beatae! Minus, quam natus. Exercitationem, eaque obcaecati.
-                </p>
-            </div>
-            <div className="service">
-                <h4>RESPONSIVE DESIGN</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptatem totam nulla vitae, numquam dolore quaerat praesentium! Consequuntur iste nisi quod nemo, qui perspiciatis quibusdam beatae! Minus, quam natus. Exercitationem, eaque obcaecati.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptatem totam nulla vitae, numquam dolore quaerat praesentium! Consequuntur iste nisi quod nemo, 
                 </p>
             </div>
         </div>
